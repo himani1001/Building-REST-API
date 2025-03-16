2ad5c784c17d2238dc4a340b78ade249fe690138
himani1001 <himanisharma15435029@gmail.com>
2025-03-16 19:25:16 +0530

    routes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const fs = require('fs');
 const users = require('./MOCK_DATA (1).json');
 
 const app = express();
 const PORT = 8000;
+
+//Middleware - Plugin
+app.use(express.urlencoded({extended: false}))
 
 //Routes
 app.get('/users', (req, res) => {
@@ -24,22 +28,34 @@ app
     .get((req, res) =>{
         const id = Number(req.params.id);
         const user = users.find((user) => user.id === id);
+        if(!user){
+            return res.json({status: 'error', message: 'User not found'});
+        }
         return res.json(user);
     })
     .patch((req, res) => {
-        return res.json({status: 'pending'});
+        const id = Number(req.params.id);
+        const userIndex = users.findIndex((user) => user.id === id);
+        users[userIndex] = { ...users[userIndex], ...req.body };
+        fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users), (err, data) => {
+            return res.json({user: users[userIndex]});
+        });
     })
     .delete((req, res) => {
-        return res.json({status: 'pending'});
-    })
+        const id = Number(req.params.id);
+        const user = users.find((user) => user.id === id);
+        users.splice(user, 1);
+        fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users), (err, data) => {
+            return res.json({ status: 'success', message: 'User deleted successfully' });
+        });
+    });
 
 app.post('/api/users', (req, res) => {
-    return res.json({status: 'pending'});
+    const body = req.body;
+    users.push({ ...body, id: users.length + 1});
+    fs.writeFile('./MOCK_DATA (1).json', JSON.stringify(users), (err, data) => {
+        return res.json({status: 'success', id: users.length});
+    });
 });
-
-app.patch('/api/users/:id', (req, res) => {
-    return res.json({status: 'pending'});
-})
-
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`))
